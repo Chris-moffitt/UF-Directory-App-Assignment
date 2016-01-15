87fa9a70046ba065c4e670b0b312878b76c358eb
Chris-moffitt <moffitt.a.chris@gmail.com>
2016-01-14 21:15:47 -0500

    Update listingController.js

--- listingController.js
+++ listingController.js
@@ -2,13 +2,41 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
   function($scope, Listings) {
     $scope.listings = Listings;
     $scope.detailedInfo = undefined;
-
-    /* 
-      Implement these functions in the controller to make your application function 
-      as described in the assignment spec. 
-     */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+    $scope.addListing = function() {
+	$scope.listings.push({'name':$scope.Name , 'code':$scope.Code , 'address':$scope.Address , 'coordinates':{'latitude':$scope.Latitude , 'longitude':$scope.Longitude}})
+	$scope.Code='';
+	$scope.Name='';
+	$scope.Address='';
+	$scope.Latitude='';
+	$scope.Longitude='';
+    };
+    $scope.deleteListing = function(name) {
+		var index = -1;		
+		var comArr = eval($scope.listings );
+		for( var i = 0; i < comArr.length; i++ ) {
+			if( comArr[i].name === name ) {
+				index = i;
+				break;
+			}
+		}
+		if( index === -1 ) {
+			alert( "Something gone wrong" );
+		}
+		$scope.listings.splice( index, 1 );
+    };
+    $scope.showDetails = function(name) {
+		var index = -1;		
+		var comArr = eval($scope.listings );
+		for( var i = 0; i < comArr.length; i++ ) {
+			if( comArr[i].name === name ) {
+				index = i;
+				break;
+			}
+		}
+		if( index === -1 ) {
+			alert( "Something gone wrong" );
+		}
+		$scope.detailedInfo=Listings[index];
+    };
   }
 ]);
